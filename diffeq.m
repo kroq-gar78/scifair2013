ae2e3c0528b46c823f32f3e7381a5e16e11f6b72
Aditya Vaidya <kroq.gar78@gmail.com>
2013-08-26 23:49:06 -0500

    Starting to add manual increments of time for eventual use in determining rates from temperature

--- diffeq.m
+++ diffeq.m
@@ -24,7 +24,19 @@ popf(3)=r(2)*pop(2)-(m(3)+r(3))*pop(3);
 popf(4)=r(3)*pop(3)-(m(4))*pop(4);
 end
 
-pop = lsode("f", [E;L;P;Ah], (t=linspace(0,800,800)'));
+t_start=0;
+t_end=800;
+t_count=t_end*2+1;
+t_step=(t_end-t_start)/t_count; % some math is wrong here; probably off by 1 or something
+t = linspace(t_start,t_end,t_count)';
+pop=zeros(size(t,1),4);
+pop(1,:)=[E;L;P;Ah];
+for n=1:size(t,1)-1
+	tmp=lsode("f", pop(n,:), t_step);
+	pop(n+1,:)=tmp;
+end
+
+%pop = lsode("f", [E;L;P;Ah], (t=linspace(0,800,1600)'));
 %[t,pop] = rk4('f',[0,50],[E;L;P;Ah;Ar;Ao]);
 
 % plot and annotate
@@ -33,3 +45,7 @@ title("Populations of stages of Aedes aegypti over time");
 xlabel("Time (days)");
 ylabel("Population");
 legend("Eggs","Larvae","Pupae","Adults");
+
+% save plot
+%print(strcat("plots/diffeq.svg"),"-dsvg")
+%print(strcat("plots/diffeq.png"),"-dpng")
