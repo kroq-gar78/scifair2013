1597dd50e817af3496b64aa37f5ac4ac3f4534f6
Aditya Vaidya <kroq.gar78@gmail.com>
2013-08-31 01:12:12 -0500

    Updated automatic regression script to work with the newer regression code

--- regress.sh
+++ regress.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+degree=2
+echo "degree=$degree"
+
 # automagically do all of the regressions (at least for the different stages)
 for i in eggs larvae pupae; do
-	sed -i -r "s/^phasename \= \"[a-zA-Z]+\"$/phasename \= \"$i\"/g" m_regress.m
-	octave m_regress.m
-	sed -i -r "s/^phasename \= \"[a-zA-Z]+\"$/phasename \= \"$i\"/g" r_regress.m
-	octave r_regress.m
+	#sed -i -r "s/^phasename \= \"[a-zA-Z]+\"$/phasename \= \"$i\"/g" m_regress.m
+	echo "m_$i"
+	octave -q --eval "m_regress(\"$i\",$degree)"
+	#sed -i -r "s/^phasename \= \"[a-zA-Z]+\"$/phasename \= \"$i\"/g" r_regress.m
+	echo "r_$i"
+	octave -q --eval "r_regress(\"$i\",$degree)"
 done
 
