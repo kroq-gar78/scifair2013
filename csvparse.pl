38eb8119db1ce95da09959acde3df450811f6790
Aditya Vaidya <kroq.gar78@gmail.com>
2013-09-10 11:24:49 -0500

    csvparse.pl: added functionality for reading gzip'd files, and it can now write to an external file

--- csvparse.pl
+++ csvparse.pl
@@ -1,18 +1,32 @@
-use 5.010;
+#use 5.010;
 use strict;
 use warnings;
 
 use Text::CSV; # in Debian package "libtext-csv-perl"
+use File::Basename;
 
 my $csv = Text::CSV->new({sep_char => ','});
+#my $out = Text::CSV->new({sep_char => ','});
 my $file = $ARGV[0] or die "Need an input file\n";
 #print $file;
 
+my @cols = (3,4,21);
+
 my @out;
 
+open(my $of, ">", "asdf.csv");
+
 my $linenum = 0;
-open(my $data, "<:encoding(utf8)", "$file");
-while (my $line = <$data> or die "Failed to read $file: $!")
+my $data;
+if(((fileparse("$file",qr/\.[^.]*/))[2]) eq ".gz")
+{
+	open($data, "gunzip -c $file |") or die "Can't open pipe to $file: $!";
+}
+else
+{
+	open($data, "<:encoding(utf8)", "$file") or die "Can't open $file: $!";
+}
+while (defined(my $line = <$data>))
 {
 	$linenum+=1;
 	chomp $line;
@@ -26,13 +40,33 @@ while (my $line = <$data> or die "Failed to read $file: $!")
 			next;
 		}
 		
+		my @tmp;
+		
+		my %colsmap = map { $_ => 1 } @cols;
+		
 		for(my $i = 0; $i < scalar @fields; $i++)
 		{
 			$fields[$i] =~ s/^\s*(.*?)\s*$/$1/;
+			if(exists($colsmap{$i}))
+			{
+				#$tmp=$tmp."$fields[$i],";
+				push @tmp,"$fields[$i]";
+				#print "$fields[$i],";
+			}
 		}
-
-		print "$fields[3],$fields[4],$fields[21]\n";
-		push @out,@fields;
+		
+		#print "$fields[3],$fields[4],$fields[21]\n";
+		#push @out,"$fields[3],$fields[4],$fields[21]\n";
+		#push @out,"$tmp";
+		#print "\n";
+		#print "@tmp\n";
+		my $status = $csv->combine(@tmp);
+		if(defined($status))
+		{
+			push @out,$csv->string();
+			#$csv->print($of,@tmp);
+			print $of $csv->string()."\n";
+		}
 		 
 	} else
 	{
@@ -40,6 +74,10 @@ while (my $line = <$data> or die "Failed to read $file: $!")
 	}
 }
 
-print "@out\n";
-
+#$csv->eof or $csv->error_diag();
 close $data;
+
+#print "@out\n";
+#$csv->print ($of,$_) for @out;
+#$csv->print($of,$out);
+close $of or die "asdf.csv: $!";
