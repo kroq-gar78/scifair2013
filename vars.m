1b1dc083e948c407ac585c7e0b99c9453e232d90
Aditya Vaidya <kroq.gar78@gmail.com>
2013-11-08 20:51:35 -0600

    vars.m: reads from CSV instead of MATLAB binary format

--- vars.m
+++ vars.m
@@ -2,12 +2,10 @@
 
 %temp=25;
 
-%E = 3000;
-E=10;
+E = 1000;
 L = 100;
 P = 100;
-%Ah = 100;
-Ah = 10;
+Ah = 1000;
 Ar = 100;
 Ao = 100;
 
@@ -19,31 +17,31 @@ Ao = 100;
 %m=[0.688,0.44,0.52,0.18,0.0043,0.41];
 
 load fitr_eggs.mat;
-reggs_p=p; # weights for transition rate of eggs
+reggs_p=csvread("fitr_eggs.csv"); # weights for transition rate of eggs
 load fitr_larvae.mat;
-rlarvae_p=p; # weights for transition rate of larvae
+rlarvae_p=csvread("fitr_larvae.csv"); # weights for transition rate of larvae
 load fitr_pupae.mat;
-rpupae_p=p; # weights for transition rate of pupae
+rpupae_p=csvread("fitr_pupae.csv"); # weights for transition rate of pupae
 
 
 load fitm_eggs.mat;
-meggs_p=p; # weights for death rate of eggs
+meggs_p=csvread("fitm_eggs.csv"); # weights for death rate of eggs
 load fitm_larvae.mat;
-mlarvae_p=p; # weights for death rate of larvae
+mlarvae_p=csvread("fitm_larvae.csv"); # weights for death rate of larvae
 load fitm_pupae.mat;
-mpupae_p=p; # weights for death rate of pupae
+mpupae_p=csvread("fitm_pupae.csv"); # weights for death rate of pupae
 load fitm_adults.mat;
-madults_p=p; # weights for death rate of adults
+madults_p=csvread("fitm_adults.csv"); # weights for death rate of adults
 
 %b=16; % number of female eggs laid per oviposition
 load fit_oviposit.mat;
-oviposit_p=p;
-b=polyval(oviposit_p,temp)/2; if(b<0); b=0; disp("Oviposition amount less than 0"); end
+oviposit_p=csvread("fitr_adults.csv");
+b=polyval(oviposit_p,temp); if(b<0); b=0; disp("Oviposition amount less than 0"); end
 
 % r={egg,larva,pupa,ovipositRate}
 %r=[.48 .14 .5 .4];
 %r=[.48 .14 .5 .1];
-r=[polyval(reggs_p,temp) polyval(rlarvae_p,temp) polyval(rpupae_p,temp) .4];
+r=[polyval(reggs_p,temp) polyval(rlarvae_p,temp) polyval(rpupae_p,temp) .15];
 for n=1:4; if(r(n)<0); r(n)=0; end; end;
 % m={egg,larva,pupa,adult}
 %m=[.688 .44 .52 .41];
