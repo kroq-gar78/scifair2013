05e8e57dde8b35df89b8b509ce70ffa5a9497c2f
Aditya Vaidya <kroq.gar78@gmail.com>
2013-11-08 22:56:26 -0600

    diffeq.m: simplified code a bit by reusing some stuff

--- diffeq.m
+++ diffeq.m
@@ -10,10 +10,11 @@ t_end=100; # days
 t_step=1; # days
 t=linspace(t_start,t_end,(t_end-t_start)/t_step+1)';
 
-#{
+
 
 pop=zeros(size(t,1),4);
 pop(1,:)=[E;L;P;Ah];
+#{
 global templist=tempfit(24.27056,2.8,18.30016,t)
 for n=1:size(t,1)-1
 	global temp = templist(n)
@@ -47,8 +48,13 @@ end
 
 #}
 
+function temp=tempfit(a,b,c,t)
+	temp=a+b*cos(2*pi*t/365.25+c);
+end
+
 function popf=f(pop,t)
 	temp=tempfit(24.27056,2.8,18.30016,t);
+	#temp=30;
 	#temp=temp;
 	vars % set the default variables
 	popf(1)=b*r(4)*pop(4)-(m(1)+r(1))*pop(1);
@@ -57,7 +63,7 @@ function popf=f(pop,t)
 	popf(4)=r(3)*pop(3)-(m(4))*pop(4);
 end
 
-pop = lsode("f", [E;L;P;Ah], t);
+pop = lsode("f", [pop(1,:)], t);
 %[t,pop] = rk4('f',[0,50],[E;L;P;Ah;Ar;Ao]);
 
 [tmp,istate,msg]=lsode("f", pop(n,:), [0,t(n+1)-t(n)]);
