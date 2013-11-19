d891c5cd55dd67876efc72e0216bfece3e30bf1c
Aditya Vaidya <kroq.gar78@gmail.com>
2013-11-18 22:56:21 -0600

    diffeq.m: converted back into the 'inefficient' method of solving the ODEs so that real temperatures can be used instead of regressed ones

--- diffeq.m
+++ diffeq.m
@@ -14,68 +14,42 @@ t=linspace(t_start,t_end,(t_end-t_start)/t_step+1)';
 
 pop=zeros(size(t,1),4);
 pop(1,:)=[E;L;P;A1];
-#{
-global templist=tempfit(24.27056,2.8,18.30016,t)
-for n=1:size(t,1)-1
-	global temp = templist(n)
-	%temp=17.8995;
-	%temp=18;
-	function popf=f(pop,t)
-		#temp=tempfit(24.27056,2.8,18.30016,t);
-		#temp=temp;
-		vars % set the default variables
-		%{
-		popf(1)=b*r(6)*pop(6)-(m(1)+r(1))*pop(1);
-		popf(2)=r(1)*pop(1)-(m(2)+r(2))*pop(2);
-		popf(3)=r(2)*pop(2)-(m(3)+r(3))*pop(3);
-		popf(4)=r(3)*pop(3)-(m(4)+r(4))*pop(4);
-		popf(5)=r(4)*pop(4)-(m(5)+r(5))*pop(5);
-		popf(6)=r(5)*pop(5)-(m(6))*pop(6);
-		%}
-		popf(1)=b*r(4)*pop(4)-(m(1)+r(1))*pop(1);
-		popf(2)=r(1)*pop(1)-(m(2)+r(2))*pop(2);
-		popf(3)=r(2)*pop(2)-(m(3)+r(3))*pop(3);
-		popf(4)=r(3)*pop(3)-(m(4))*pop(4);
-	end
-	
-	[tmp,istate,msg]=lsode("f", pop(n,:), [0,t(n+1)-t(n)]);
-	if(istate!=2)
-		disp(msg)
-	end
-	%pop(n+1,:)=tmp(2,:);
-	pop(n+1,:)=tmp(2,1:size(pop,2));
-end
-
-#}
 
 function temp=tempfit(a,b,c,t)
 	temp=a+b*cos(2*pi*t/365.25+c);
 end
 
-function popf=f(pop,t)
-	%temp=tempfit(18.0,6.7,9.2,t);
-	temp=33; % 18, 21, 24, 27, 30, 33
-	#temp=temp;
-	vars % set the default variables
+%global templist=tempfit(24.27056,2.8,18.30016,t)
+for n=1:size(t,1)-1
+	%global temp = templist(n)
+	%temp=17.8995;
+	%temp=18;
+	function popf=f(pop,t)
+		%temp=tempfit(18.0,6.7,9.2,t);
+		temp=33; % 18, 21, 24, 27, 30, 33
+		#temp=temp;
+		vars % set the default variables
+		
+		popf(1)=b*r(4)*pop(4)-(m(1)+r(1)*(1-0.63))*pop(1);
+		popf(2)=r(1)*pop(1)*(1-0.63)-0.01*pop(2)*pop(2)-(m(2)+r(2))*pop(2);
+		popf(3)=r(2)*pop(2)-(m(3)+r(3))*pop(3);
+		popf(4)=r(3)*pop(3)/2-(m(4))*pop(4);
+		
+		%{
+		popf(1)=b*(r(4)*pop(4)+r(5)*pop(5))-(m(1)+r(1)*(1-0.63))*pop(1);
+		popf(2)=r(1)*pop(1)*(1-0.63)-0.01*pop(2)*pop(2)-(m(2)+r(2))*pop(2);
+		popf(3)=r(2)*pop(2)-(m(3)+r(3))*pop(3);
+		popf(4)=r(3)*pop(3)/2-(m(4)+r(4))*pop(4);
+		popf(5)=r(4)*pop(4)-m(4)*pop(5);
+		%}
+	end
 	
-	popf(1)=b*r(4)*pop(4)-(m(1)+r(1)*(1-0.63))*pop(1);
-	popf(2)=r(1)*pop(1)*(1-0.63)-0.01*pop(2)*pop(2)-(m(2)+r(2))*pop(2);
-	popf(3)=r(2)*pop(2)-(m(3)+r(3))*pop(3);
-	popf(4)=r(3)*pop(3)/2-(m(4))*pop(4);
-	
-	%{
-	popf(1)=b*(r(4)*pop(4)+r(5)*pop(5))-(m(1)+r(1)*(1-0.63))*pop(1);
-	popf(2)=r(1)*pop(1)*(1-0.63)-0.01*pop(2)*pop(2)-(m(2)+r(2))*pop(2);
-	popf(3)=r(2)*pop(2)-(m(3)+r(3))*pop(3);
-	popf(4)=r(3)*pop(3)/2-(m(4)+r(4))*pop(4);
-	popf(5)=r(4)*pop(4)-m(4)*pop(5);
-	%}
-end
-
-[pop,istate,msg] = lsode("f", [pop(1,:)], t);
-%[t,pop] = rk4('f',[0,50],[E;L;P;Ah;Ar;Ao]);
-if(istate!=2)
-	disp(msg)
+	[tmp,istate,msg]=lsode("f", pop(n,:), [t(n),t(n+1)]);
+	if(istate!=2)
+		disp(msg)
+	end
+	%pop(n+1,:)=tmp(2,:);
+	pop(n+1,:)=tmp(2,1:size(pop,2));
 end
 
 % plot and annotate
