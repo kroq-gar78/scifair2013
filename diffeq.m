bb2c2c08fc851998c59d4c83bbe983d807a974e8
Aditya Vaidya <kroq.gar78@gmail.com>
2013-11-25 20:27:21 -0600

    diffeq.m: moved tempfit function to the top for organization, switched back from even time step to staggered (the ones in the input list)

--- diffeq.m
+++ diffeq.m
@@ -7,17 +7,24 @@
 
 lsode_options("minimum step size",1.E-4) % set time step to 0.0001 so that the time step doesn't become too small (also for efficiency); 0.0001 days ~= 6 seconds, so shouldn't affect the result
 
-t_start=0; # days
-%t_start=templist(1,1);
-%t_end=templist(size(templist(:,1),1));
-t_end=2000; # days
+function temp=tempfit(a,b,c,t)
+	temp=a+b*cos(2*pi*t/365.25+c);
+	%temp=fita+fitb*cos(2*pi*t/365.25+fitc);
+end
+
+%t_start=0; # days
+t_start=templist(1,1);
+t_end=templist(size(templist(:,1),1));
+%t_end=2000; # days
 t_step=0.5; # days
 teven=linspace(t_start,t_end,(t_end-t_start)/t_step+1)'; % evenly spaced time intervals
 
-%t=templist(1:t_end/t_step,1);
-%tlist=templist(1:size(templist(:,1),1),1);
-tlist=teven;
-%t=linspace(templist(1,1),templist(size(templist(:,1),1),1),1);
+%tlist=templist(1:t_end/t_step,1);
+tlist=templist(1:end,1);
+%tlist=teven;
+
+%temps=tempfit(26.89,3.82,10.1188,tlist); % double hump
+temps=templist(:,2);
 
 pop=zeros(size(teven,1),4);
 pop(1,:)=[E;L;P;A1];
@@ -27,11 +34,6 @@ global fita = 18.0
 global fitb = 0.7
 global fitc = 9.2
 %}
-
-function temp=tempfit(a,b,c,t)
-	temp=a+b*cos(2*pi*t/365.25+c);
-	%temp=fita+fitb*cos(2*pi*t/365.25+fitc);
-end
 
 function popf=f(pop,t,templist)
 	%temp=tempfit(18.0,6.7,9.2,t);
@@ -70,9 +72,6 @@ function popf=f(pop,t,templist)
 	%}
 end
 
-temps=tempfit(21.2755144118492,18.7014981438,9.2866007993,tlist);
-%temps=templist(:,2);
-
 %{
 %global templist=tempfit(24.27056,2.8,18.30016,t)
 for n=1:size(tlist,1)-1
@@ -93,16 +92,17 @@ for n=1:size(tlist,1)-1
 end
 %}
 
-fd = @(pop,t)f(pop,t,[tlist temps;(tlist(end)+lsode_options("minimum step size")),temps(end)]); % add extra line to the array so that the interpolation doesn't try to evaluate out of bounds
+tmparray=[(tlist(1)-lsode_options("minimum step size")),temps(1);tlist temps;(tlist(end)+lsode_options("minimum step size")),temps(end)]; % add extra line to the array so that the interpolation doesn't try to evaluate out of bounds
+fd = @(pop,t)f(pop,t,tmparray);
 %fd = @(pop,t)f(pop,t,[teven temps]);
-[pop,istate,msg]=lsode(fd,pop(1,:),teven);
+[pop,istate,msg]=lsode(fd,pop(1,:),tlist);
 if(istate!=2)
 	disp(msg)
 end
 
 % plot and annotate
 %plot(tlist,pop)
-plot(teven,pop(:,4))
+plot(tlist,pop(:,4))
 %plot(t,[pop(:,1:3) pop(:,4)+pop(:,5)])
 %title("Populations of stages of Aedes aegypti over time");
 %xlabel("Time (days)");
@@ -114,3 +114,6 @@ legend("Adults");
 % save plot; this MIGHT be broken, but it could also be because of error-related outputs earlier in the program
 %print(strcat("plots/diffeq.svg"),"-dsvg")
 %print(strcat("plots/diffeq.png"),"-dpng")
+
+% save the population
+%csvwrite("pop_INSERT-DATE.csv",[teven interp1(tmparray(:,1),tmparray(:,2),tlist) pop])
