7eeceb3046384bbc3711f08f51257e9355f24627
Aditya Vaidya <kroq.gar78@gmail.com>
2014-02-27 23:15:39 -0600

    LOTS of changes, mainly stuff that wasn't committed when primary execution code was moved into main.m. Most important addition is changeable alpha (density dependent mortality) values

--- diffeq.m
+++ diffeq.m
@@ -5,39 +5,16 @@
 % equation: 18+6.7*sin(2*pi*t/365.24+9.2)
 %tempeq=
 
-lsode_options("minimum step size",1.E-4) % set time step to 0.0001 so that the time step doesn't become too small (also for efficiency); 0.0001 days ~= 6 seconds, so shouldn't affect the result
+function pop=diffeq(timetemp,initpop)
 
-function temp=tempfit(a,b,c,t)
-	temp=a+b*cos(2*pi*t/365.25+c);
-	%temp=fita+fitb*cos(2*pi*t/365.25+fitc);
-end
+tlist = timetemp(:,1);
+temps = timetemp(:,2);
+pop=zeros(size(tlist,1),4); % initialize blank population matrix
+pop(1,:)=initpop; % initialize population at t=0
 
-t_start=0; # days
-%t_start=templist(1,1);
-%t_end=templist(size(templist(:,1),1));
-t_end=1200; # days
-t_step=1; # days
-teven=linspace(t_start,t_end,(t_end-t_start)/t_step+1)'; % evenly spaced time intervals
-
-%tlist=templist(1:t_end/t_step,1);
-%tlist=templist(1:end,1);
-tlist=teven;
-
-%temps=tempfit(26.89,3.82,10.1188,tlist); % double hump
-%temps=tempfit(18,6.7,9.2,tlist); % sinusoidal for Buenos Aires (from Otero, et al. 2006)
-%temps=templist(:,2);
-temps=zeros(size(teven,1),1)+27; % constant temperature of 27
-
-pop=zeros(size(teven,1),4);
-pop(1,:)=[E;L;P;A1];
-
-%{
-global fita = 18.0
-global fitb = 0.7
-global fitc = 9.2
-%}
-
+% differential equations
 function popf=f(pop,t,templist)
+%function popf=f(t,pop,templist)
 	%temp=tempfit(18.0,6.7,9.2,t);
 	%temp=tempfit(fita,fitb,fitc);
 	%temp=33; % 18, 21, 24, 27, 30, 33
@@ -58,7 +35,11 @@ function popf=f(pop,t,templist)
 	%}
 	temp=interp1(templist(:,1),templist(:,2),t,"spline");
 	
-	vars % set the default variables
+	vars % calculate variables from temperature
+	
+	%alphas=[0 0.01;220 0.01;235 0.02]
+	alphas=[0 0.01; 200 0.01; 220 0.015; 230 0.02; 235 0.02; 400 0.015; 600 0.01; 1200+1 0.01];
+	alpha=interp1(alphas(:,1),alphas(:,2),t,"linear");
 	
 	popf(1)=b*r(4)*pop(4)-(m(1)+r(1)*(1-gamma))*pop(1);
 	popf(2)=r(1)*pop(1)*(1-gamma)-alpha*(pop(2)^2)-(m(2)+r(2))*pop(2);
@@ -67,8 +48,8 @@ function popf=f(pop,t,templist)
 	%[t temp popf]
 	
 	%{
-	popf(1)=b*(r(4)*pop(4)+r(5)*pop(5))-(m(1)+r(1)*(1-0.63))*pop(1);
-	popf(2)=r(1)*pop(1)*(1-0.63)-0.01*pop(2)*pop(2)-(m(2)+r(2))*pop(2);
+	popf(1)=b*(r(4)*pop(4)+r(5)*pop(5))-(m(1)+r(1)*(1-gamma))*pop(1);
+	popf(2)=r(1)*pop(1)*(1-gamma)-0.01*pop(2)*pop(2)-(m(2)+r(2))*pop(2);
 	popf(3)=r(2)*pop(2)-(m(3)+r(3))*pop(3);
 	popf(4)=r(3)*pop(3)/2-(m(4)+r(4))*pop(4);
 	popf(5)=r(4)*pop(4)-m(4)*pop(5);
@@ -95,24 +76,17 @@ for n=1:size(tlist,1)-1
 end
 %}
 
-tmparray=[(tlist(1)-lsode_options("minimum step size")),temps(1);tlist temps;(tlist(end)+lsode_options("minimum step size")),temps(end)]; % add extra line to the array so that the interpolation doesn't try to evaluate out of bounds
+tmparray=[(tlist(1)-lsode_options("minimum step size")),temps(1);tlist temps;(tlist(end)+lsode_options("minimum step size")),temps(end)]; % add extra lines (beginning and end) to the array so that the interpolation doesn't try to evaluate out of bounds
+%fd = @(t,pop)f(t,pop,tmparray);
+%[t_junk,pop]=ode45(fd,tlist,pop(1,:));
 fd = @(pop,t)f(pop,t,tmparray);
-%fd = @(pop,t)f(pop,t,[teven temps]);
+
 [pop,istate,msg]=lsode(fd,pop(1,:),tlist);
 if(istate!=2)
+	disp(istate)
 	disp(msg)
 end
 
-% plot and annotate
-%plot(tlist,pop)
-plot(tlist,pop(:,4))
-%plot(t,[pop(:,1:3) pop(:,4)+pop(:,5)])
-%title("Populations of stages of Aedes aegypti over time");
-%xlabel("Time (days)");
-%ylabel("Population");
-%legend("Eggs","Larvae","Pupae","Adults (1)","Adults (2)");
-%legend("Eggs","Larvae","Pupae","Adults");
-legend("Adults");
 
 % save plot; this MIGHT be broken, but it could also be because of error-related outputs earlier in the program
 %print(strcat("plots/diffeq.svg"),"-dsvg")
@@ -120,3 +94,5 @@ legend("Adults");
 
 % save the population
 %csvwrite("pop_INSERT-DATE.csv",[teven interp1(tmparray(:,1),tmparray(:,2),tlist) pop])
+
+end
